8f39f0e32f654e5f558fa8152a0011a5b854b30f
Lady Luck <S.ruiz@live.iup.edu>
2019-07-27 22:46:57 -0400

    adding root files

---
+++ assets/javascript/liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+// concert-this
+// spotify-this-song
+// movie-this
+// do-what-it-says
